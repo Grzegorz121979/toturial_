aa7260b117a03d50959c2f5269e0e19d90af1fcb
Grzegorz121979 <pociechagrzegorz@outlook.com>
2024-05-03 21:31:23 +0200

    struct

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,50 @@
-use rand::Rng;
+use std::io::{self, Write};
+
+/* struct Rectangle {
+    width: u32,
+    height: u32
+} */
 
 fn main() {
-    let mut vec: Vec<i32> = Vec::new();
-
-    let random_int_vec: &Vec<i32> = even_generate_vec(&mut vec);
+    /* let rect1 = Rectangle {
+        width: 30,
+        height: 50
+    }; */
     
-    println!("{:?}", random_int_vec);
+    let mut w = String::new();
+    let mut h = String::new();
+
+    print!("Width: ");
+    io::stdout()
+        .flush()
+        .unwrap();
+    io::stdin()
+        .read_line(&mut w)
+        .expect("Failed read line");
+
+    let w: u32 = w.trim()
+                    .parse()
+                    .unwrap();
+
+    print!("Height: ");
+    io::stdout()
+        .flush()
+        .unwrap();
+    io::stdin()
+        .read_line(&mut h)
+        .expect("Failed read line");
+
+    let h: u32 = h.trim()
+                    .parse()
+                    .unwrap();
+    
+    println!("The area of rectangle is {} square pixels", area(w, h));
 }
 
-fn even_generate_vec(v: &mut Vec<i32>) -> &Vec<i32> {
-    let mut count: i32 = 0;
-
-    while count <= 10 {
-        let rand_number: i32 = rand::thread_rng().gen_range(1..=11);
-        v.push(rand_number);
-        count += 1;
-    }
-    v.retain(|x: &i32| x % 2 == 0);
-    v
+fn area(a: u32, b: u32) -> u32 {
+    a * b
 }
+
+/* fn area(rectangle: &Rectangle) -> u32 {
+    rectangle.height * rectangle.width
+} */
