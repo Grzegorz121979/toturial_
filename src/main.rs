bb5b3d1717f98637ca1a0c2c29fb77f42805d776
Grzegorz121979 <pociechagrzegorz@outlook.com>
2024-05-04 11:36:36 +0200

    next commit

--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,19 @@
-use std::io::{self, Write};
-
-/* struct Rectangle {
+struct Rectangle {
     width: u32,
     height: u32
-} */
+}
+
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.height * self.width
+    }
+}
 
 fn main() {
-    /* let rect1 = Rectangle {
-        width: 30,
-        height: 50
-    }; */
-    
-    let mut w = String::new();
-    let mut h = String::new();
+    let rect1 = Rectangle {
+        width: 20,
+        height: 60
+    };
 
-    print!("Width: ");
-    io::stdout()
-        .flush()
-        .unwrap();
-    io::stdin()
-        .read_line(&mut w)
-        .expect("Failed read line");
-
-    let w: u32 = w.trim()
-                    .parse()
-                    .unwrap();
-
-    print!("Height: ");
-    io::stdout()
-        .flush()
-        .unwrap();
-    io::stdin()
-        .read_line(&mut h)
-        .expect("Failed read line");
-
-    let h: u32 = h.trim()
-                    .parse()
-                    .unwrap();
-    
-    println!("The area of rectangle is {} square pixels", area(w, h));
+    println!("The area of rectangles is {} square pixels", rect1.area());
 }
-
-fn area(a: u32, b: u32) -> u32 {
-    a * b
-}
-
-/* fn area(rectangle: &Rectangle) -> u32 {
-    rectangle.height * rectangle.width
-} */
